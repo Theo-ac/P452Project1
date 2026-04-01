ba87ec2007b0726e69ee047655f4af6bd167fc8b
Theo-ac <125707117+Theo-ac@users.noreply.github.com>
2026-04-01 17:01:23 -0500

    Add quantum circuit measurement function

---
+++ backend.py
@@ -0,0 +1,12 @@
+import qiskit
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+from qiskit_aer.primitives import SamplerV2
+from qiskit.visualization import plot_histogram
+def measure_Circuit(circuit):
+    sampler = SamplerV2()
+    job = sampler.run([circuit], shots=1024)
+    result_ideal = job.result()
+    counts_ideal = result_ideal[0].data.meas.get_counts()
+    print('Counts(ideal):', counts_ideal)
+    return counts_ideal
