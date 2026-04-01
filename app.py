7ccd94b9f516f0aa7f83fedcc7c95da3eda998a4
Theo-ac <125707117+Theo-ac@users.noreply.github.com>
2026-04-01 17:00:46 -0500

    Update import path for measure_Circuit function

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import streamlit as st
-from P452_Project1 import measure_Circuit
+from backend import measure_Circuit
 st.title("Theo's 10-Qubit Universal Quantum Computer Simulator")
+x = st.slider("Pick a value", 0, 10, 5)
+st.write("You picked:", x)
