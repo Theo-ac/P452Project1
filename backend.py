fa1c0afbf35a11b37ec4ce59eada8da7829bfc44
Theo-ac <125707117+Theo-ac@users.noreply.github.com>
2026-04-02 15:26:59 -0500

    Update measurement syntax in backend.py

--- backend.py
+++ backend.py
@@ -28,8 +28,8 @@ def teleportation(n_qubits, theta):
     qc.cx(0,1)
     qc.h(0)
     qc.barrier(label="Bell State Measurement")
-    qc.measure(1, 1)
-    qc.measure(0, 0)
+    qc.measure(1, qc.clbits[1])
+    qc.measure(0, qc.clbits[0])
     with qc.if_test((qc.clbits[1], 1)):
         qc.x(n_qubits-1)
     with qc.if_test((qc.clbits[0], 1)):
