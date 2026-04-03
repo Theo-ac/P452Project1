e07fff0c40ea301a85a62bf3a0b167c9f8821227
Theo-ac <125707117+Theo-ac@users.noreply.github.com>
2026-04-02 19:39:25 -0500

    Import Statevector from qiskit.quantum_info

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 from backend import measure_Circuit, teleportation, hubbard, GHZ_Circuit
+from qiskit.quantum_info import Statevector
 from qiskit.visualization import plot_histogram
 st.title("Theo's 10-Qubit Universal Quantum Computer Simulator")
 n_qubits = st.slider("How many qubits do you want?", 1, 10, 5)
