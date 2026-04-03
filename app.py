e14052d74e83a52b86b99df28e79f7b57ca3ddb4
Theo-ac <125707117+Theo-ac@users.noreply.github.com>
2026-04-03 01:30:30 -0500

    Refine probability threshold for detection
    Change the threshold for full probability detection from 1e-3 to 1e-6.

--- app.py
+++ app.py
@@ -91,9 +91,9 @@ else:
         ax.set_ylabel("Probability of |0100>")
         ax.set_title("Time Evolution of Probability")
         st.pyplot(fig)
-        indices = np.where(np.abs(np.array(probs) - 1) < 1e-3)[0]
+        indices = np.where(np.abs(np.array(probs) - 1) < 1e-6)[0]
         if len(indices) != 0:
-            st.write("The first time at full probability is: ", indices[0])
+            st.write("The first time at full probability is: ", times[indices[0]])
     else:
         st.write("You'll want 4 qubits to simulate a 2-site Fermi-Hubbard Model :]")
 
